d5f35813d8f1d773ea842750bbcef8e61bdebf74
Vadym Zhylochkin <Vadym.Zhylochkin.IR.2024@lpnu.ua>
2024-09-18 10:09:42 +0300

    Add files via upload

--- part2.py
+++ part2.py
@@ -1,3 +1,5 @@
+import math
+
 #ТАБЛИЦЯ 1
 
 #Змінні
@@ -16,8 +18,8 @@ print("Не дорівнює: ", (is_employee != is_employer))
 is_sunny = True
 is_raining = False
 
-print("Логічне не. Сонячно: ", not is_sunny, 
-      "; Йде дощ: ", not is_raining)
+print("Логічне не. Сонячно: ", not is_sunny, "; ",
+      "Йде дощ: ", not is_raining)
 
 
 
@@ -27,3 +29,6 @@ z = 9.761
 
 print("Результат виразу: ", 
       pow(z, 6) + 13*pow(z, 4) + 7*pow(z, 3) + 14*pow(z, 2) - 2*z + 21)
+
+# Або
+# z**6 + 13*(z**4) + 7*(z**3) + 14*(z**2) - 2*z + 21
